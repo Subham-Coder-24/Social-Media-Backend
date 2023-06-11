a0f885c9ddfcaa817361669ab4896a4a8bba7746
Subham Swarup Pradhan <85586258+Subham-Coder-24@users.noreply.github.com>
2023-06-12 00:49:10 +0530

    first commit

--- index.js
+++ index.js
@@ -16,10 +16,8 @@ app.use(
   })
 );
 app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET,OPTIONS,PATCH,DELETE,POST,PUT"
