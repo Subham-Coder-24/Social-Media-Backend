4d52ca97dcbb5f6d7cfe9e06512282335d540f69
Subham Swarup Pradhan <85586258+Subham-Coder-24@users.noreply.github.com>
2023-06-11 23:35:38 +0530

    3 commit

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.use(
     credentials: true,
   })
 );
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  next();
+});
 
 app.use("/posts", route);
 
