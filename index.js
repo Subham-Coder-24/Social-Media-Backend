2278d0b2ad0df25c7ff00435b6727c7f5d71ec5c
Subham Swarup Pradhan <85586258+Subham-Coder-24@users.noreply.github.com>
2023-06-12 00:53:18 +0530

    first commit

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ import cloudinary from "cloudinary";
 
 const app = express();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(
   cors({
     origin: "http://localhost:3000",
@@ -24,6 +22,8 @@ app.use((req, res, next) => {
   );
   next();
 });
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use("/posts", route);
 
